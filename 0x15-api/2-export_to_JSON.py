471773e4c4e0d50927ba0b6e3f8ff17273d2ba81
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-22 16:45:31 +0300

    mule

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' makes a get request to a REST API '''
+import requests
+import json
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = argv[1]
+    user_data = requests.get(url + 'users/' + user_id).json()
+    task_data = requests.get(url + 'todos').json()
+    user_name = user_data['name']
+
+    # Write tasks to JSON file
+    tasks = []
+    for task in task_data:
+        if str(task['userId']) == user_id:
+            tasks.append({
+                'task': task['title'],
+                'completed': task['completed'],
+                'username': user_name
+            })
+        with open('{}.json'.format(user_id), mode="w") as file:
+            json.dump({user_id: tasks}, file)
