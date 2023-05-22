6c8a6eb1e610bb551dd2dca4baa03c134df519d5
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-22 15:30:29 +0300

    mule

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,25 @@
-#!/usr/bin/python3
-''' makes a get request to a REST API '''
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    i = 0
-    j = 0
-    n = []
-    user_id = argv[1]
-    user_data = requests.get(url + 'users/' + user_id).json()
-    task_data = requests.get(url + 'todos').json()
-    m = user_data['name']
+
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Retrieve user data
+    user_data = requests.get(f"{base_url}/users/{employee_id}").json()
+    employee_name = user_data["name"]
+
+    # Retrieve task data
+    task_data = requests.get(f"{base_url}/todos?userId={employee_id}").json()
+
+    # Calculate task progress
+    num_completed_tasks = sum(1 for task in task_data if task["completed"])
+    total_num_tasks = len(task_data)
+
+    # Print progress report
+    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_num_tasks}):")
     for task in task_data:
-        if str(task['userId']) == user_id:
-            if str(task["completed"]) == 'True':
-                n.append(str(task["title"]))
-                j = j + 1
-            i = i + 1
-    print("Employee {} is done with tasks({}/{}):".format(m, j, i))
-    for k in n:
-        print('\t {}'.format(k))
+        if task["completed"]:
+            print(f"\t{task['title']}")
