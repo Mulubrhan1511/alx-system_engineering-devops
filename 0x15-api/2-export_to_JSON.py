9a7912bf3262d401bff226e0c2f2550e3326aecc
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-24 21:19:18 +0300

    mule

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-''' makes a get request to a REST API '''
+'''
+A script to export data in the JSON format.
+'''
+
 import json
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_id = argv[1]
-    user_data = requests.get(url + 'users/' + user_id).json()
-    task_data = requests.get(url + 'todos').json()
-    user_name = user_data['name']
-
-    # Write tasks to JSON file
-    tasks = []
-    for task in task_data:
-        if str(task['userId']) == user_id:
-            tasks.append({
-                'task': task['title'],
-                'completed': task['completed'],
-                'username': user_name
-            })
-        with open('{}.json'.format(user_id), mode="w") as file:
-            json.dump({user_id: tasks}, file)
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
+    todo = requests.get(url, verify=False).json()
+    name = user.get('username')
+    t = [{"task": t.get("title"),
+          "username": name,
+          "completed": t.get("completed")} for t in todo]
+    bj = {}
+    bj[uid] = t
+    with open("{}.json".format(uid), 'w') as filejs:
+        json.dump(bj, filejs)
